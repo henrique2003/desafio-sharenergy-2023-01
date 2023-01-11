aae09b24109f155e806aaa4e5999f2e3d7671d84
henrique2003 <henrique.de.melo.cristioglu@gmail.com>
2023-01-11 13:25:42 -0300

    chore: add eslint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'standard-with-typescript',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/space-before-blocks': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off'
+  }
+}
